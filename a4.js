6e0b49997a19fda7f859ce07c86e06f32dde374e
Alex Russell <amrussell@smu.edu>
2015-02-09 13:06:18 -0600

    made a4.js functional

--- a4.js
+++ a4.js
@@ -1,8 +1,15 @@
-document.body.style.background = "#eeeeee";
-alert(document.title);
-var email = "amrussell@smu.edu";
-var reg = new RegExp("^[a-zA-Z0-9.-_]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,4}$");
-if(email.match(reg) != null)
-	alert(email + " is a valid email");
-else
-	alert(email + " is not a valid email");
+function changeBackground() {
+	document.body.style.background = "#eeeeee";
+}
+function alertPageTitle() {
+	alert(document.title);
+}
+
+function emailRegex() {
+	var email = "amrussell@smu.edu";
+	var reg = new RegExp("^[a-zA-Z0-9.-_]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,4}$");
+	if(email.match(reg) != null)
+		alert(email + " is a valid email");
+	else
+		alert(email + " is not a valid email");
+}
