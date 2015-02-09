48786dcffed9298146fd5ec173daf5dfa458bb09
Alex Russell <amrussell@smu.edu>
2015-02-09 12:57:39 -0600

    finished a4.js

--- a4.js
+++ a4.js
@@ -2,3 +2,7 @@ document.body.style.background = "#eeeeee";
 alert(document.title);
 var email = "amrussell@smu.edu";
 var reg = new RegExp("^[a-zA-Z0-9.-_]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,4}$");
+if(email.match(reg) != null)
+	alert(email + " is a valid email");
+else
+	alert(email + " is not a valid email");
